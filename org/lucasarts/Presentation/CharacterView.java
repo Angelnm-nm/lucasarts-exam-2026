b388135ec9ecf65acac10d44397458b1d78070ef
MNA <angel.martinnavas2323@gmail.com>
2026-03-05 20:13:45 +0100

    he hecho que se pudan ver todos los personajes

--- org/lucasarts/Presentation/CharacterView.java
+++ org/lucasarts/Presentation/CharacterView.java
@@ -9,28 +9,23 @@ import lucasarts.Domain.GetCharacter;
 
 public class CharacterView {
     CharactersData charactersData;
-    public static void printcharacter (){
+
+    public static void printcharacter() {
         GetCharacter getCharacter = new GetCharacter((CharactersData) new CharacterDataSource(new CharacterMemLocalDataSource()));
-        CharacterModel characterModel1 = new CharacterModel("1233","gg","100","atractivo","20");
+        CharacterModel characterModel1 = new CharacterModel("1233", "gg", "100", "atractivo", "20");
         getCharacter.execute(characterModel1);
     }
-    public static void printcharacters (CharacterModel characterModel){
+
+    public static void printcharacters(CharacterModel characterModel) {
         GetCharacter getCharacter = new GetCharacter((CharactersData) new CharacterDataSource(new CharacterMemLocalDataSource()));
         getCharacter.execute(characterModel);
 
     }
-    public static void deleteCharacters (CharacterModel characterModel){
-        CharacterModel characterModel1 = new CharacterModel("22","jeb","30","cuidado","4");
-        DeleteCharacter deleteCharacter =new DeleteCharacter((CharactersData) new CharacterDataSource(new CharacterMemLocalDataSource()));
+
+    public static void deleteCharacters(CharacterModel characterModel) {
+        CharacterModel characterModel1 = new CharacterModel("22", "jeb", "30", "cuidado", "4");
+        DeleteCharacter deleteCharacter = new DeleteCharacter((CharactersData) new CharacterDataSource(new CharacterMemLocalDataSource()));
         deleteCharacter.delete(characterModel1);
 
-        }
     }
-
-
-
-
-
-
-
 }
